96a6075849a0dfc4ab52a6dbb636a0d9e3393d49
HarshitaSingh47 <harshitasingh47@gmail.com>
2020-09-30 23:51:51 +0530

    Video 2

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Leads extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Show Leads</h1>
+            </div>
+        )
+    }
+}
+
+export default Leads
+
+
+
